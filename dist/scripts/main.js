b9fdbe1754936bbd73ef21ccc8afd268c5f26396
arepenko <a.repenko@market-logic.com.ua>
2021-05-17 14:22:39 +0300

    fix slider

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".standarts_mainblock-content").slick({infinite:!0,slidesToShow:2,slidesToScroll:1,responsive:[{breakpoint:577,settings:{slidesToShow:1}}]})});
+$(document).ready(function(){$(".standarts_mainblock-content").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,speed:800,adaptiveHeight:!0})});
