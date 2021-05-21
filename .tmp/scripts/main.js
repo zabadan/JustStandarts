813f4290e9280dd022bf9eeb86cd723c032d0dc7
arepenko <a.repenko@market-logic.com.ua>
2021-05-21 07:43:30 +0300

    remove folder tmp

--- .tmp/scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.standarts_mainblock-content').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 800,
-    adaptiveHeight: true
-  });
-});
